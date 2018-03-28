c2d1bb1b6f5ae3010b9046aa7a858c38b9e94a16
Julia Cluceru <julia.cluceru@ucsf.edu>
2018-03-28 13:16:48 -0700

    created get_perf and call_perf, works well.

---
+++ Rscript/call_getPerfData.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+
+## want this script to be take in arguments 
+## [1] /pathtobnumtnum/bnumtnum.txt 
+## [2] /pathtooutput/outputfilename.txt 
+## [3] measure = median, percent25, percent75, mean, sd, min, max, percent10, percent90, mode, skewness, kurtosis, sum 
+## [4] optional, headerlabel = vol(cc),adc,fa, nadc, nfa
+
+# test if there is at least one argument: if not, return an error
+if (length(args)<=1) {
+  stop("At least 2 arguments must be supplied: 
+       [1] /pathtobnumtnum/bnumtnum.csv
+       [2] /pathtooutput/outputfilename.csv 
+       [3] optional, headerlabel = ph_nonlin, phn_nonlin... etc. functionality to be added soon.
+       note: if you choose datatype = ev, you cannot call headerlabel.", call.=FALSE)
+} else if (length(args)==2) {
+  # default output file
+  args[3] = NA
+}
+
+source("/home/sf673542/pushed_scripts/Rscript/get_perf_biopsy_data.R")
+bnum_tnum_df = read.csv(args[1], header=F)
+colnames(bnum_tnum_df)=c("bnum", "tnum", "DUMMY")
+data = get_perf_biopsy(bnum_tnum_df, headerlabel=args[4])
+write.table(data, args[2], sep=",")
+
+
--- Rscript/get_perf_biopsy_data.R
+++ Rscript/get_perf_biopsy_data.R
@@ -1,61 +1,161 @@
 
 library(dplyr)
 ## if we have biopsyabel input and we do have headerlabel input, we will use this: 
-get_perf_biopsy_withheader=function(bnum_tnum_df, measure, headerlabel){
-  data=data.frame()
+
+#######################################################################################################################################
+
+get_perf_biopsy_noheader=function(bnum_tnum_df){
+data = data.frame()
   for(i in 1:dim(bnum_tnum_df)[1]){
-   ## here we're going to read the text file for perfusion 
+   #for(i in c(20,30)){
     y=paste("/data/RECglioma/", bnum_tnum_df$bnum[i], "/", bnum_tnum_df$tnum[i], sep="")
     tnum_files = list.files(y)
     if ("perf_biopsy" %in% tnum_files && "roi_analysis" %in% tnum_files){
-      setwd(paste(y, "/svk_roi_analysis", sep=""))
-      svk_files=list.files(pattern="csv")
-      diffu_csv = paste(bnum_tnum_df$tnum[i],  "_roi_adcfa1000.csv", sep="")
-      ## only if the csv we're looking for exists do we move forward with extracting the data 
-      if (diffu_csv %in% svk_files){
-        tnum_roi_adcfa1000=read.table(diffu_csv, sep=",", header=T)
-        
-        ## here we are going to get the measure block we are looking for 
-        tnum_roi_adcfa1000_measure = tnum_roi_adcfa1000[grep(measure, as.character(tnum_roi_adcfa1000$measure)),]
-        
-        ## here we are going to calculate the number of biopsy just from this data 
-        nrow = dim(tnum_roi_adcfa1000_measure)[1]
-        
-        ## here we are going to extract only the biopsy information rows 
-        
-        tnum_roi_adcfa1000_measure = data.frame(tnum_roi_adcfa1000_measure)
-        if ("nawm" %in% tnum_roi_adcfa1000_measure$roi.label[1:10]){
-          tnum_roi_adcfa1000_m_biopsy = tnum_roi_adcfa1000_measure[12:nrow,]
+      setwd(paste(y, "/perf_biopsy", sep=""))
+      ## getting the nonlinear fit data 
+      curve_nonlin_files=list.files(pattern="curve_nonlinfit.txt")
+      if(length(curve_nonlin_files)>0){
+        curve_nonlin_files_split = strsplit(curve_nonlin_files, split="_", fixed = FALSE, perl = FALSE, useBytes = FALSE)
+        vialid=c()
+        data_nonlin=data.frame()
+        for(j in 1:length(curve_nonlin_files_split)){
+          vialid[j]=curve_nonlin_files_split[[j]][2]
+          nonlin_file = paste(bnum_tnum_df$tnum[i], "_", vialid[j], "_ave_curve_nonlinfit.txt", sep = "")
+          nonlin_table=read.table(nonlin_file)
+          row.names(nonlin_table)=nonlin_table$V1
+          nonlin_table=t(nonlin_table)
+          nonlin_table=data.frame(bnum=bnum_tnum_df$bnum[i], tnum=bnum_tnum_df$tnum[i],
+                                  vialid=vialid[j], nonlin_table)
+          data_nonlin=bind_rows(data_nonlin, nonlin_table[2,])
         }
-        else {
-          tnum_roi_adcfa1000_m_biopsy = tnum_roi_adcfa1000_measure[11:nrow,]
+        ## getting the nonparametric data 
+        curve_nonparam_files=list.files(pattern="curve_nonparam.txt")
+        curve_nonparam_files_split = strsplit(curve_nonparam_files, split="_", fixed = FALSE, perl = FALSE, useBytes = FALSE)
+        data_nonparam=data.frame()
+        for(j in 1:length(curve_nonparam_files_split)){
+          vialid[j]=curve_nonparam_files_split[[j]][2]
+          nonparam_file = paste(bnum_tnum_df$tnum[i], "_", vialid[j], "_ave_curve_nonparam.txt", sep = "")
+          nonparam_table=read.table(nonparam_file)
+          row.names(nonparam_table)=nonparam_table$V1
+          nonparam_table=t(nonparam_table)
+          #nonparam_table=data.frame(bnum=bnum_tnum_df$bnum[i], tnum=bnum_tnum_df$tnum[i],
+           #                       vialid=vialid[j], nonparam_table)
+          data_nonparam=bind_rows(data_nonparam, nonparam_table[2,])
         }
-        
-        
-        ## and here we are getting the specific header measurement
-        ## we have two possible conditions; the header we're looking for exists or doesn't exist in the header (e.g. fse might not be present)
-        if(headerlabel %in% colnames(tnum_roi_adcfa1000_m_biopsy)){
+        data_i = bind_cols(data_nonlin, data_nonparam)
+        colnames(data_i)=c("bnum", "tnum", "vialid",
+                          "ph_nonlin", "phn_nonlin",
+                         "cbv_nonlin", "cbvn_nonlin",
+                         "recov_nonlin", "recovn_nonlin",
+                         "rf_nonlin", "rfn_nonlin",
+                         "ttp_nonlin", "fwhm_nonlin", "rsquare_nonlin", 
+                         ## and the nonparam data: 
+                         "ph_nonpar", "phn_nonpar", "pbs_nonpar", 
+                         "pbsn_nonpar","recov_nonpar", "recovn_nonpar",
+                         "acorr_nonpar")
+        data = bind_rows(data, data_i)
+      }
+      else{
+        data=data
+      }
+      
+      }
+      else{
+        data = data
+      }
+  }
+  return(data)
+}
+
+#######################################################################################################################################
+#######################################################################################################################################
+
+
+get_perf_biopsy_withheader=function(bnum_tnum_df, headerlabel){
+  data = data.frame()
+  for(i in 1:dim(bnum_tnum_df)[1]){
+#   for (i in c(1,20, 21,30)){
+    y=paste("/data/RECglioma/", bnum_tnum_df$bnum[i], "/", bnum_tnum_df$tnum[i], sep="")
+    tnum_files = list.files(y)
+    if ("perf_biopsy" %in% tnum_files && "roi_analysis" %in% tnum_files){
+      setwd(paste(y, "/perf_biopsy", sep=""))
+      ## getting the nonlinear fit data 
+      curve_nonlin_files=list.files(pattern="curve_nonlinfit.txt")
+      if(length(curve_nonlin_files)>0){
+        curve_nonlin_files_split = strsplit(curve_nonlin_files, split="_", fixed = FALSE, perl = FALSE, useBytes = FALSE)
+        vialid=c()
+        data_nonlin=data.frame()
+        for(j in 1:length(curve_nonlin_files_split)){
+          vialid[j]=curve_nonlin_files_split[[j]][2]
+          nonlin_file = paste(bnum_tnum_df$tnum[i], "_", vialid[j], "_ave_curve_nonlinfit.txt", sep = "")
+          nonlin_table=read.table(nonlin_file)
+          row.names(nonlin_table)=nonlin_table$V1
+          nonlin_table=t(nonlin_table)
+          nonlin_table=data.frame(bnum=bnum_tnum_df$bnum[i], tnum=bnum_tnum_df$tnum[i],
+                                  vialid=vialid[j], nonlin_table)
+          data_nonlin=bind_rows(data_nonlin, nonlin_table[2,])
+        }
+        ## getting the nonparametric data 
+        curve_nonparam_files=list.files(pattern="curve_nonparam.txt")
+        curve_nonparam_files_split = strsplit(curve_nonparam_files, split="_", fixed = FALSE, perl = FALSE, useBytes = FALSE)
+        data_nonparam=data.frame()
+        for(j in 1:length(curve_nonparam_files_split)){
+          vialid[j]=curve_nonparam_files_split[[j]][2]
+          nonparam_file = paste(bnum_tnum_df$tnum[i], "_", vialid[j], "_ave_curve_nonparam.txt", sep = "")
+          nonparam_table=read.table(nonparam_file)
+          row.names(nonparam_table)=nonparam_table$V1
+          nonparam_table=t(nonparam_table)
+          #nonparam_table=data.frame(bnum=bnum_tnum_df$bnum[i], tnum=bnum_tnum_df$tnum[i],
+          #                       vialid=vialid[j], nonparam_table)
+          data_nonparam=bind_rows(data_nonparam, nonparam_table[2,])
+        }
+        data_i = bind_cols(data_nonlin, data_nonparam)
+        colnames(data_i)=c("bnum", "tnum", "vialid",
+                           "ph_nonlin", "phn_nonlin",
+                           "cbv_nonlin", "cbvn_nonlin",
+                           "recov_nonlin", "recovn_nonlin",
+                           "rf_nonlin", "rfn_nonlin",
+                           "ttp_nonlin", "fwhm_nonlin", "rsquare_nonlin", 
+                           ## and the nonparam data: 
+                           "ph_nonpar", "phn_nonpar", "pbs_nonpar", 
+                           "pbsn_nonpar","recov_nonpar", "recovn_nonpar",
+                           "acorr_nonpar")
+        data = bind_rows(data, data_i)
+        if(headerlabel %in% colnames(data)){
           exactheaderlabel=paste("^", headerlabel,"$", sep="")
-          h=grep(exactheaderlabel, colnames(tnum_roi_adcfa1000_m_biopsy))
-          tnum_roi_adcfa1000_m_r_headl= data.frame(tnum_roi_adcfa1000_m_biopsy[,1:4], headerlabel = tnum_roi_adcfa1000_m_biopsy[,h]) 
-          colnames(tnum_roi_adcfa1000_m_r_headl)=c(colnames(tnum_roi_adcfa1000_m_r_headl)[1:4], headerlabel)
+          h=grep(exactheaderlabel, colnames(data))
+          data_headl= data.frame(data[,1:3], headerlabel = data[,h]) 
+          colnames(data_headl)=c(colnames(data_headl)[1:3], headerlabel)
+          data=data_headl
         }
         else{
-          tnum_roi_adcfa1000_m_r_headl= data.frame(tnum_roi_adcfa1000_m_biopsy[,1:4], headerlabel = NA) 
-          colnames(tnum_roi_adcfa1000_m_r_headl)=c(colnames(tnum_roi_adcfa1000_m_r_headl)[1:4], headerlabel)
+          data = data.frame(data[,1:3], headerlabel = NA) 
+          colnames(data)=c(colnames(data)[1:3], headerlabel)
         }
-        
-        ## here we are binding the data with the previous measurement 
-        data = rbind(data, tnum_roi_adcfa1000_m_r_headl)
       }
       else{
         data=data
       }
     }
     else{
-      data=data
+      data = data
+      print(i)
     }
-    
-  }
   return(data)
+  }
+}
+
+#######################################################################################################################################
+#######################################################################################################################################
+
+get_perf_biopsy = function(bnum_tnum_df, headerlabel){
+  if(is.na(headerlabel)){
+    ## want to create the data frame like this 
+    get_perf_biopsy_noheader(bnum_tnum_df)
+  }
+  else{
+    print("Sorry, functionality of with_header will be added soon.")
+    print("Please use no header and gather all data.")
+    #get_perf_biopsy_withheader(bnum_tnum_df, headerlabel)
+  }
 }
